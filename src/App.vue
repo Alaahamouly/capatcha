b38128cc5eb34a5fbbe5f7546012427e830ff9dc
aelhamouly@sure.com.sa <aelhamouly@sure.com.sa>
2023-08-03 21:26:21 +0300

    init capatcha example app

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <h3>Capatcha generator</h3>
+    <Capatcha />
+  </div>
+</template>
+
+<script>
+import Capatcha from './components/Capatcha.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Capatcha
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
